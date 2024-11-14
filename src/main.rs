a0726591485a72a707d0d63c40881a78ede9fad5
OuOu2021 <1113117424@qq.com>
2024-11-14 16:20:28 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,31 @@ use anyhow::{Context, Result};
 use clap::Parser;
 use lingua::LanguageDetectorBuilder;
 use rand::seq::SliceRandom;
-use rand::SeedableRng;
+use rand::{random, SeedableRng};
 use word_list::WordList;
 
 fn main() -> Result<()> {
     let mut term = Term::stdout();
     let config = Config::parse();
-    let input = std::fs::read_to_string(&config.path).context(format!(
-        "failed to get input from {:?}",
-        config.path.to_str().context("illegal unicode file name"),
+    let word_list = std::fs::read_to_string(&config.path).context(format!(
+        "failed to get word list from \"{}\"",
+        config.path.to_str().context("illegal unicode file name")?,
     ))?;
-    let mut input: WordList = serde_json::from_str(&input)?;
+    let mut input: WordList = serde_json::from_str(&word_list)?;
 
     let mut words = input.words;
-    if input.language.is_none() {
-        let detector = LanguageDetectorBuilder::from_languages(&LANGUAGES).build();
-        let detected = words.join(" ");
-        input.language = detector.detect_language_of(detected);
+    match input.language {
+        None => {
+            let detector = LanguageDetectorBuilder::from_languages(&LANGUAGES).build();
+            let detected = words.join(" ");
+            input.language = detector.detect_language_of(detected);
+        }
+        _ => {}
     }
 
     if !config.dont_shuffle && config.mode == Mode::Dictate {
-        let d = std::time::SystemTime::now()
-            .duration_since(std::time::SystemTime::UNIX_EPOCH)
-            .context("Duration since UNIX_EPOCH failed")?;
-        let mut rng = rand::rngs::StdRng::seed_from_u64(d.as_secs());
+        let seed: u64 = random();
+        let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
         words.shuffle(&mut rng);
     }
 
@@ -52,8 +53,8 @@ fn main() -> Result<()> {
             println!("About to quit.");
 
             std::thread::sleep(std::time::Duration::from_secs(1));
+            Ok(())
         }
-        Mode::Read => read(&mut speaker, &words)?,
-    };
-    Ok(())
+        Mode::Read => read(&mut speaker, &words),
+    }
 }
